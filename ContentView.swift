6f7d6c4377bc23703c7963b02b11bfd67e7d5efa
Louie2026 <boatfloor@icloud.com>
2026-04-21 10:50:06 -0500

    added calender (auto complete helped)

--- ContentView.swift
+++ ContentView.swift
@@ -1,7 +1,8 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selectedDates: Set<DateComponents> = []
     var body: some View {
-        
+        MultiDatePicker("Workout Date Selector", selection: $selectedDates)
     }
 }
