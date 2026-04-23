344f8d80beb79b36dac510ba9808eb277d1b8a31
Louie2026 <boatfloor@icloud.com>
2026-04-23 11:00:17 -0500

    added base notfication

--- File.swift
+++ File.swift
@@ -6,6 +6,16 @@
 //
 
 import SwiftUI
+import UserNotifications
+
+func requestPermission() {
+    UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+        if let error = error {
+            print("Notification permission error: \(error)")
+        }
+        print("Permission granted: \(granted)")
+    }
+}
 
 struct AddAssignmentView: View {
     
