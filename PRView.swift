7df657d384679e4c2156f580e109ab5456f05ff3
swcw1234 <swysocki9451@stu.d214.org>
2026-04-29 11:05:07 -0500

    i changed the backrounds

--- PRView.swift
+++ PRView.swift
@@ -3,27 +3,36 @@ import SwiftUI
 struct PRView: View {
     @State private var assignments: [String] = []
     var body: some View {
-        
-        Button("Request Notfication Acesses") {
-            requestPermission()
-               
-        }
-        NavigationView {
-            List(assignments, id: \.self) { item in
-                Text(item)
-            }
-            .navigationTitle("Personal Records")
-            .toolbar {
-                NavigationLink(destination: AddWorkoutView(assignments: $assignments)) {
-                    Image(systemName: "plus")
-                    Text ("Personal Record")
-                    Spacer()
-                        .font(.largeTitle)
-                        .bold()
+        ZStack {
+            Color.gray.ignoresSafeArea()
+            VStack(spacing: 16) {
+                
+                Button("Request Notfication Acesses") {
+                    requestPermission()
                     
                 }
+                NavigationView {
+                    List(assignments, id: \.self) { item in
+                        Text(item)
+                    }
+                    .navigationTitle("Personal Records")
+                    .toolbar {
+                        NavigationLink(destination: AddWorkoutView(assignments: $assignments)) {
+                            Image(systemName: "plus")
+                            Text ("Personal Record")
+                            Spacer()
+                                .font(.largeTitle)
+                                .bold()
+                                Color.gray.ignoresSafeArea()
+                                VStack(spacing: 16) {
+                                    
+                                }
+                            }
+                        }
+                    }
+                }
+                
             }
         }
     }
-}
 
