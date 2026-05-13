c266b080405478666e2aa7d5846f203b7a1c00dd
Paul <psuarezkwan9396@stu.d214.org>
2026-05-13 11:05:49 -0500

    made leves to the sets along with using the Menu code

--- File.swift
+++ File.swift
@@ -10,13 +10,10 @@ import SwiftUI
 struct File: View {
     @State var exercies: [String]  = ["PUSHUPS", "SQUATES","LUNGES","BENCH PRESSES","PULLUPS","CARDIO", "LATERAL RAISES", "DEADLIFTS", "HAMMER CURLS", "JUMMPING JACKS", "DUMBELL ROWS", "OVERHEAD PRESS", "BENT OVER ROWS"]
     
-    @State var Edice8 = ["8","5","6","5","6","8","7"]
+    @State var Edice10 = ["8","6","5","10","7","9","10","4"]
     @State var Ndice15 = ["12","14","15","15","3","16","8","17","8","9","10","11","12","13","15","14"]
     @State var Hdice35 = ["22","21","12","4","5","12","24","13","16","18","17","18","19","20","10","11","12","13","15","14","23","25","26","27,","28","29","34","31","32","35"]
     @State var VHdice1055 = ["125","150","130","151", "101","130","130","155","50, 63","126","73","68","73", "100","1055" ]
-    
-    
-    @State var dice6 = [1,2,3,4,5,6]
     @State var dice = [""]
     var body: some View {
         ZStack {
@@ -43,16 +40,24 @@ struct File: View {
                     .foregroundStyle(.white)
                     .bold()
                     .font(.largeTitle)
-                
+                Spacer()
             }
             VStack{
-//                Button("easy"){
-//                   dice = Edice8
-//                }
-//                .font(.headline)
-                
-                Button("Sets"){
-                    Edice8.shuffle()
+                Menu {
+                    Button("easy"){
+                        dice = Edice10
+                    }
+                    Button("Normal"){
+                        dice = Ndice15
+                    }
+                    Button("Hard"){
+                        dice = Hdice35
+                    }
+                    Button("Very Hard"){
+                        dice = VHdice1055
+                    }
+                } label: {
+                    Text("mode")
                 }
                 .foregroundStyle(.white)
                 .font(.largeTitle)
@@ -60,12 +65,26 @@ struct File: View {
                 .background(.blue)
                 .cornerRadius(15)
                 .scaledToFit()
-                Text(Edice8.randomElement()!)
-                    .foregroundStyle(.white)
-                    .bold()
+                
+                
+                .font(.headline)
+                
+                if dice != [""]{
+                    Button("Sets"){
+                        dice.shuffle()
+                    }
+                    .foregroundStyle(.blue)
                     .font(.largeTitle)
-            }
-            .offset(y: 150)
+                    .bold()
+                    .background(.white)
+                    .cornerRadius(15)
+                    .scaledToFit()
+                    Text(dice.randomElement()!)
+                        .foregroundStyle(.white)
+                        .bold()
+                        .font(.largeTitle)
+                }
+                                }
         }
     }
 }
